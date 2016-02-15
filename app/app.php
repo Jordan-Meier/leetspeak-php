8ed34a2a000b64d3697d8b47e9d054fead7a6132
Joseph Karasek and Jordan Meier <joseph.karasek@gmail.com>
2016-02-15 11:46:59 -0800

    add UI with twig

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/LeetspeakGenerator.php";
 
     $app = new Silex\Application();
 
@@ -7,8 +8,17 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() {
-        return "Home";
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/translate_submit", function() use ($app) {
+        $my_LeetspeakGenerator = new LeetspeakGenerator;
+        $result = $my_LeetspeakGenerator->translate($_GET['phrase']);
+
+        return $app['twig']->render('index.html.twig', array(
+            'result' => $result
+        ));
     });
 
     return $app;
